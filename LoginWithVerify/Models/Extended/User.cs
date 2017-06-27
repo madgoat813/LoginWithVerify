d80de32738dd5ce773141dcfb135c49b2e9e3872
madgoat813 <madgoat813@DESKTOP-OM096FN>
2017-06-27 13:31:18 -0500

    Validation Added

--- LoginWithVerify/Models/Extended/User.cs
+++ LoginWithVerify/Models/Extended/User.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
-namespace LoginWithVerify.Models.Extended
+namespace LoginWithVerify.Models
 {
-    public class User
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
     {
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name = "First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "UserName")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "UserName required")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm password and password do not match")]
+        public string ConfirmPassword { get; set; }
+
     }
 }
